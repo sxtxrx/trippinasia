17439234ee503d8b1a2c710e976ca3d9aca82f8b
sxxrx <sxtxrxfor.pg@gmail.com>
2021-04-15 22:14:26 +0900

    jquery追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,7 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
+require('jquery')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -16,6 +17,5 @@ require("channels")
 
 //= require rails-ujs
 //= require activestorage
-//= require jquery
 //= require turbolinks
 //= require_tree .
