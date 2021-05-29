fceab437c79609731325a37c5f45d22e6120a232
sxxrx <sxtxrxfor.pg@gmail.com>
2021-05-29 22:11:49 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true
-  validates :profile, presence: true
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)\w{6,}\z/
+  validates :password,
+            format: { with: VALID_PASSWORD_REGEX }
+
+  with_options presence: true do
+    validates :name
+    validates :profile
+  
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, { presence: true, uniqueness: { case_sensitive: false } }
+  end
+
+
+
 
   has_many :prototypes, dependent: :destroy
   has_many :comments, dependent: :destroy
